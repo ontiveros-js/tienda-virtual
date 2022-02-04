b052983b8daafc9549723caf9f111e4ec68256dd
douglas ontiveros <douglasontiveros1408@gmail.com>
2022-02-04 20:46:10 -0300

    Updates

---
+++ precache-manifest.bdd1c981829c7697e48bc722e237f141.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c73990f19f2b0c0d3e83f0ca2211794",
+    "url": "/tienda-virtual/index.html"
+  },
+  {
+    "revision": "3ae6b3e562e32ae52420",
+    "url": "/tienda-virtual/static/css/main.b77dbd1a.chunk.css"
+  },
+  {
+    "revision": "99f3971956e507ad0d0e",
+    "url": "/tienda-virtual/static/js/2.ceab2ed7.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/tienda-virtual/static/js/2.ceab2ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae6b3e562e32ae52420",
+    "url": "/tienda-virtual/static/js/main.793c2495.chunk.js"
+  },
+  {
+    "revision": "7ddd668cb45cc4cf5cf3",
+    "url": "/tienda-virtual/static/js/runtime-main.5f718da6.js"
+  },
+  {
+    "revision": "71145515323e376971e1802aa000b90e",
+    "url": "/tienda-virtual/static/media/bt_add_to_cart.71145515.svg"
+  },
+  {
+    "revision": "1a348c2540a98537a3257bdc67bd5667",
+    "url": "/tienda-virtual/static/media/email.1a348c25.svg"
+  },
+  {
+    "revision": "5d3b853e10a449f36ca6a3f9e8ea95a6",
+    "url": "/tienda-virtual/static/media/flechita.5d3b853e.svg"
+  },
+  {
+    "revision": "23ae2bbff5a3a0e37d238045dd321760",
+    "url": "/tienda-virtual/static/media/icon_menu.23ae2bbf.svg"
+  },
+  {
+    "revision": "cd2cb126a70d30fd14c927571fd1920e",
+    "url": "/tienda-virtual/static/media/icon_shopping_cart.cd2cb126.svg"
+  },
+  {
+    "revision": "4db98c8925bc0ef833d3a32307652a83",
+    "url": "/tienda-virtual/static/media/logo_yard_sale.4db98c89.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tienda-virtual/precache-manifest.bdd1c981829c7697e48bc722e237f141.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tienda-virtual/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
